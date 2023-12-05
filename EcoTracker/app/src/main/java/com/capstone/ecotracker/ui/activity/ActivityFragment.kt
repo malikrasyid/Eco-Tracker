e383d074d1648c3e610ac2c829ad11106633ebcc
malikrasyid <116499018+malikrasyid@users.noreply.github.com>
2023-12-05 13:33:33 +0700

    Upload File Project

---
+++ EcoTracker/app/src/main/java/com/capstone/ecotracker/ui/activity/ActivityFragment.kt
@@ -0,0 +1,39 @@
+package com.capstone.ecotracker.ui.activity
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.capstone.ecotracker.databinding.FragmentActivityBinding
+
+class ActivityFragment : Fragment() {
+
+    private var _binding: FragmentActivityBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val activityViewModel =
+            ViewModelProvider(this).get(ActivityViewModel::class.java)
+
+        _binding = FragmentActivityBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ EcoTracker/app/src/main/java/com/capstone/ecotracker/ui/report/ReportFragment.kt
@@ -0,0 +1,38 @@
+package com.capstone.ecotracker.ui.report
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.capstone.ecotracker.databinding.FragmentReportBinding
+
+class ReportFragment : Fragment() {
+
+    private var _binding: FragmentReportBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val reportViewModel =
+            ViewModelProvider(this).get(ReportViewModel::class.java)
+
+        _binding = FragmentReportBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
